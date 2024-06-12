61c9552bd8be2cd33dbb3402dd0e171cf2828de3
Paco Valdez <paco@cube.dev>
2024-06-12 16:02:00 -0700

    Fail gracefully when no file is found

--- lkml2cube/main.py
+++ lkml2cube/main.py
@@ -9,6 +9,7 @@ from typing_extensions import Annotated
 
 app = typer.Typer()
 
+
 @app.callback()
 def callback():
     """
@@ -18,59 +19,91 @@ def callback():
     #             "Use lkml2cube --help to see usage."))
     pass
 
+
 @app.command()
 def cubes(
-        file_path: Annotated[str, typer.Argument(help="The path for the file to read")],
-        parseonly: Annotated[bool, typer.Option(help=("When present it will only show the python"
-                                                      " dict read from the lookml file"))] = False,
-        outputdir: Annotated[str, typer.Option(help="The path for the output files to be generated")] = '.',
-        printonly: Annotated[bool, typer.Option(help="Print to stdout the parsed files")] = False,
-    ):
+    file_path: Annotated[str, typer.Argument(help="The path for the file to read")],
+    parseonly: Annotated[
+        bool,
+        typer.Option(
+            help=(
+                "When present it will only show the python"
+                " dict read from the lookml file"
+            )
+        ),
+    ] = False,
+    outputdir: Annotated[
+        str, typer.Option(help="The path for the output files to be generated")
+    ] = ".",
+    printonly: Annotated[
+        bool, typer.Option(help="Print to stdout the parsed files")
+    ] = False,
+):
     """
     Generate cubes-only given a LookML file that contains LookML Views.
     """
-    
+
     lookml_model = file_loader(file_path)
+
+    if lookml_model is None:
+        typer.echo(f"No files were found on path: {file_path}")
+        raise typer.Exit()
+
     if parseonly:
         typer.echo(pprint.pformat(lookml_model))
         return
-    
+
     cube_def = parse_view(lookml_model)
     cube_def = generate_cube_joins(cube_def, lookml_model)
-    
+
     if printonly:
         typer.echo(yaml.dump(cube_def, allow_unicode=True))
         return
-    
+
     write_files(cube_def, outputdir=outputdir)
-    
 
 
 @app.command()
 def views(
-        file_path: Annotated[str, typer.Argument(help="The path for the explore to read")],
-        parseonly: Annotated[bool, typer.Option(help=("When present it will only show the python"
-                                                      " dict read from the lookml file"))] = False,
-        outputdir: Annotated[str, typer.Option(help="The path for the output files to be generated")] = '.',
-        printonly: Annotated[bool, typer.Option(help="Print to stdout the parsed files")] = False,
-    ):
+    file_path: Annotated[str, typer.Argument(help="The path for the explore to read")],
+    parseonly: Annotated[
+        bool,
+        typer.Option(
+            help=(
+                "When present it will only show the python"
+                " dict read from the lookml file"
+            )
+        ),
+    ] = False,
+    outputdir: Annotated[
+        str, typer.Option(help="The path for the output files to be generated")
+    ] = ".",
+    printonly: Annotated[
+        bool, typer.Option(help="Print to stdout the parsed files")
+    ] = False,
+):
     """
     Generate cubes-only given a LookML file that contains LookML Views.
     """
-    
+
     lookml_model = file_loader(file_path)
+
+    if lookml_model is None:
+        typer.echo(f"No files were found on path: {file_path}")
+        raise typer.Exit()
+
     if parseonly:
         typer.echo(pprint.pformat(lookml_model))
         return
-    
+
     cube_def = parse_explores(lookml_model)
 
     if printonly:
         typer.echo(yaml.dump(cube_def, allow_unicode=True))
         return
-    
+
     write_files(cube_def, outputdir=outputdir)
+
 
 if __name__ == "__main__":
     app()
-
